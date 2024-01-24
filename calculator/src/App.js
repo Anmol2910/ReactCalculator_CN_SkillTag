81e3dec6fff08b618c076137e227205b8b55b3e1
Anmol2910 <anmolawasthi2910@gmail.com>
2024-01-24 17:19:01 +0530

    calculater

---
+++ calculator/src/App.js
@@ -0,0 +1,10 @@
+import Calculator from "./component/calculator";
+import './index.css';
+
+export default function App() {
+  return (
+    <div className="wrapper">
+      <Calculator />
+    </div>
+  );
+}
---
+++ calculator/src/component/utils/evaluate.js
@@ -0,0 +1,29 @@
+export const evaluate = ({ previousOperand, currentOperand, operator }) => {
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+  
+    let result = "";
+  
+    switch (operator) {
+      case "+":
+        result = prev + current;
+        break;
+      case "-":
+        result = prev - current;
+        break;
+      case "*":
+        result = prev * current;
+        break;
+      case "/":
+        result = prev / current;
+        break;
+      case "%":
+        result = (prev / 100) * current;
+        console.log(result);
+        break;
+      default:
+        result = 0;
+    }
+    return result.toString();
+  };
+  
